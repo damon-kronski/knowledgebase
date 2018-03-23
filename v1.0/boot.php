3fde1ebf4dba1ac8e27267ca283c5cfba97a8be9
Melvin Suter <melvinsuter@Melvins-Air.fritz.box>
2018-03-23 21:58:14 +0100

    removed old files

--- v1.0/boot.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include(__DIR__.'/lib/help.php');
-include(__DIR__.'/lib/mysql.php');
-include(__DIR__.'/config/db.php');
-include(__DIR__.'/lib/admin.php');
-
-include(__DIR__.'/lib/showpage.php');
--- v1.0/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-MySQL_Helper::$CONFIG['username'] = 'rwkb';
-MySQL_Helper::$CONFIG['password'] = 'dTypphK9XtysFjVsajXCgPem';
-MySQL_Helper::$CONFIG['dbname'] = 'kb';
-MySQL_Helper::$CONFIG['server'] = 'localhost';
